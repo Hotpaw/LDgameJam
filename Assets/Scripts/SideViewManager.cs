038cba702113376b8f06b3f62a098457497bf6be
GVBååth <gustav.baath02@gmail.com>
2023-09-30 16:38:55 +0200

    tempcollision

---
+++ Assets/Scripts/SideViewManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideViewManager : MonoBehaviour
+{
+
+
+    public void ReturnToTopDown()
+    {
+
+    }
+    public void CreateNewObstacle()
+    {
+
+    }
+}
