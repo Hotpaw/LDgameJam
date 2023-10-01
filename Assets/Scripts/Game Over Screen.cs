46bdb2d6408b3aa75e37c2cb7bf78a3f5c112837
GVBååth <gustav.baath02@gmail.com>
2023-10-01 16:30:45 +0200

    bottle anim

---
+++ Assets/Scripts/Game Over Screen.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverScreen : MonoBehaviour
+{
+
+    private void OnEnable()
+    {
+        Time.timeScale = 0;
+    }
+}
